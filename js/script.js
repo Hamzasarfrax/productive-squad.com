d751e33829a399be8e2653449560358ae9e4b8d3
Hamzasarfrax <hamzasarfraz862@gmail.com>
2022-11-07 13:04:53 -0800

    work

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+function submition(event){
+    event.preventDefault()
+    let required=document.querySelector(".required");
+let form_control=document.querySelector(".form-control")
+if(form_control.value <5){
+   required.innerHTML=" is required"
+}
+    
+}
+
+
+
+
+console.log(particle)
+
+
