100faba2ce43947efdcf209d10bbe82d824d3e18
Hamzasarfrax <hamzasarfraz862@gmail.com>
2022-11-17 19:33:09 +0500

    working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 
+function validation(){
+ 
+  let input=document.getElementsByClassName("form-control");
+  let required=document.getElementsByClassName("required");
+  for(let i=0;i<input.length;i++){
+    for(let k=0;k<required.length;k++){
+      if(input[i].value<5){
+        required[i].innerHTML="field is required"
+       
+      }
+      else{
+        alert("sucessfull")
+        required[i].innerHTML=""
+      let item=input.value;
+      let formdetails={item}
+
+      console.log(JSON.stringify(formdetails))
+      }
+    }
+  }
+}
+
+
 document.onscroll=function(){
 	let text = document.getElementById("animated_scroll")
 let scrol = document.body.scrollTop || document.documentElement.scrollTop;
@@ -8,9 +31,18 @@ if (scrol === 0) {
   }, 800)
 }
 else if (scrol > 0) {
-  text.classList.remove("active")
+  // document.getElementById("animated_scroll").classList.remove("active")
 }
 }
 
 
 
+
+let loading=document.getElementById("loader")
+
+function loader(){
+setTimeout(()=>{
+  loading.style.display="none"
+},4000)
+
+}
